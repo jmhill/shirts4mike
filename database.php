d09464c06b8236935e7c21e8c1b34ca8197d1c89
Justin Hill <justinmhill@gmail.com>
2015-01-09 01:07:23 -0500

    Connect project to database

--- database.php
+++ database.php
@@ -1,11 +1,20 @@
 <?php
 
 try {
-	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=8889","root","root");
-	var_dump($db);
+	$db = new PDO("mysql:host=localhost;dbname=shirts4mike;port=8889","root","treehouse");
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("SET NAMES 'utf8'");
 } catch (Exception $e) {
 	echo "Could not connect to the database.";
 	exit;
 }
 
-echo "Woo-hoo!";
+try {
+	$results = $db->query("SELECT name, price FROM products ORDER BY sku ASC");
+} catch (Exception $e) {
+	echo "Data could not be retrieved";
+	exit;
+}
+
+echo "<pre>";
+var_dump($results->fetchAll());
