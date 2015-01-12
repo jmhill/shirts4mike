e1df8514071903c76be85fd692f30524297a30f6
Justin Hill <justinmhill@gmail.com>
2015-01-11 21:17:36 -0500

    Update config file to hold database information constants

--- inc/config.php
+++ inc/config.php
@@ -5,3 +5,9 @@
 
 	define("BASE_URL","/");
 	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
+
+	define("DB_HOST","localhost");
+	define("DB_NAME","shirts4mike");
+	define("DB_PORT", "3306");
+	define("DB_USER", "root");
+	define("DB_PASS", "treehouse");
